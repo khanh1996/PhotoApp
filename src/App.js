97b799426efab07e5efdb86178e85fb45080e98e
khanhvb <khanhvb@smartosc.com>
2020-12-23 09:42:11 +0700

    update form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+  
+import { unwrapResult } from '@reduxjs/toolkit';
+import productApi from './api/productApi';
+import { getMe } from './app/userSlice';
+import SignIn from './features/Auth/pages/SignIn';
+import firebase from 'firebase';
+import React, { Suspense, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import './App.scss';
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+
+// Lazy load - Code splitting
+const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
+
+  const handleButtonClick = () => {
+    alert("handleButtonClick");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="photo-app">
+    <Suspense fallback={<div>Loading ...</div>}>
+      <BrowserRouter>
+        <Header />
+
+        <Button onClick={handleButtonClick}>Fetch Product List</Button>
+
+        <Switch>
+          <Redirect exact from="/" to="/photos" />
+
+          <Route path="/photos" component={Photo} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
+  </div>
   );
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Col, Container, Row } from 'reactstrap';
+import './Header.scss';
+
+Header.propTypes = {};
+
+function Header() {
+  return (
+    <header className="header">
+      <Container>
+        <Row className="justify-content-between">
+          <Col xs="auto">
+            <NavLink
+              exact
+              className="header__link header__title"
+              to="/photos"
+            >
+              Easy Frontend
+            </NavLink>
+          </Col>
+          <Col xs="auto">
+            <NavLink
+              exact
+              className="header__link"
+              to="/sign-in"
+              activeClassName="header__link--active"
+            >
+              Sign In
+            </NavLink>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
